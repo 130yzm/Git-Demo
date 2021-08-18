a53b71602f06a00934e7bd8e83f9a7a5479f2a03
yzm <2862431899@qq.com>
2021-08-19 00:49:52 +0800

    第一次更改

---
+++ demo-module1/src/test/test1.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @user: y'z'm
+ * @date: 2021.08.19 0:46
+ * @Description:
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
